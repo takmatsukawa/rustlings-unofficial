a94f5f0083b674c2e26e0b1ecc928581e6b55690
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2022-06-15 22:09:27 +0900

    Use borrowed types for arguments

--- exercises/design-pattern/use-borrowed-types-for-arguments.rs
+++
@@ -1,8 +0,0 @@
-// use-borrwed-types-for-arguments.rs
-// Execute the command `rustlings hint use-borrowed-types-for-arguments` for a hint.
-
-// I AM NOT DONE
-
-fn main() {
-    println!("Hello {}");
-}
